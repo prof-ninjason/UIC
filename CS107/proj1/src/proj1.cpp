486ae86e7fa177fc8e03a6c0896266d9cd1c7a4b
jcrochon <jcrochon@achilles.cc.uic.edu>
2016-04-26 15:13:51 -0500

    Added CS 107 Project 1

---
+++ CS107/proj1/src/proj1.cpp
@@ -0,0 +1,26 @@
+/* This program is to do something
+ *
+ * Writeen by:   Some Person
+ * on:           Some Date
+ * for:          Some Class
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Begin Execution" << endl;
+
+	// Print information about the author
+	cout << "Author: Jason C. Rochon" << endl;
+	cout << "UIN:    123456789" << endl;
+	cout << "CS 107 - Summer 2015" << endl;
+	cout << "Lab Assignment 1" << endl;
+	cout << "Lab Time: Wednesday at 10am" << endl;
+
+	cout << "End Execution" << endl;
+	return 0;
+}
+
